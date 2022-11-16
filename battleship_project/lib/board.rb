8bac0f138037da793793809aa9683a538a6a86e6
Wilson Wu <wuyecheng738@gmail.com>
2022-11-16 16:16:09 -0500

    finished part1

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,3 +1,33 @@
 class Board
-  
+  attr_reader :size
+  def initialize(num)
+    @grid = Array.new(num) { Array.new(num, :N)}
+    @size = num * num
+  end
+
+  def [](position)
+    row, col = position
+    @grid[row][col]
+
+  end
+
+  def []=(position, val)
+    row, col = position
+    @grid[row][col] = val
+  end
+
+  def num_ships
+    @grid.flatten.count { |ele| ele == :S}
+  end
+
+  def attack(position)
+    
+
+
+
+
 end
+
+
+
+
