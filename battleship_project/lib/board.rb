ec869ddc011334f8fb73741453adb7924834b285
Wilson Wu <wuyecheng738@gmail.com>
2022-11-16 17:32:13 -0500

    part2 battleship

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,5 +1,11 @@
 class Board
   attr_reader :size
+  def self.print_grid(grid)
+    grid.each do |row|
+      puts row.join(" ")
+    end
+  end
+
   def initialize(num)
     @grid = Array.new(num) { Array.new(num, :N)}
     @size = num * num
@@ -21,11 +27,49 @@ class Board
   end
 
   def attack(position)
+    if self[position] == :S 
+      self[position] = :H
+      puts "you sunk my battleship"
+      return true
+    else 
+      self[position] = :X 
+      return false
+    end
+  end
+
+  def place_random_ships
+    total_ships = @size * 0.25
     
+    while self.num_ships < total_ships
+      rand_col = rand(0...@grid.length)
+      rand_row = rand(0...@grid.length)
+      pos = [rand_row, rand_col]
+      self[pos] = :S 
+    end
+  end
+
+  def hidden_ships_grid
+    @grid.map do |row|
+      row.map do |ele|
+        if ele == :S 
+          :N 
+        else
+          ele
+        end
+      end
+    end
+  end
 
 
+  def cheat
+    Board.print_grid(@grid)
+  end
 
+  def print
+    Board.print_grid(self.hidden_ships_grid)
+  end
 
+  
 end
 
 
